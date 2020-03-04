3154a8e4bb21d56b1f88c72ffd823cb9add80a36
BinotaLIU <binota@binota.org>
2020-03-04 10:39:49 +0800

    extract data-bag

---
+++ data-bag.js
@@ -0,0 +1,77 @@
+const fetchPage = require('./fetch-page').default
+
+exports.default = class DataBag {
+  constructor () {
+    this.events = []
+    this.birthdays = {}
+  }
+
+  async updateAll() {
+    return Promise.all([
+      this.updateEvents(),
+      this.updateBirthdays()
+    ])
+  }
+
+  async updateEvents () {
+    this.events = await this.fetchEvents()
+  }
+
+  async updateBirthdays () {
+    this.birthdays = await this.fetchBirthdays()
+  }
+
+  async fetchEvents () {
+    return (await fetchPage('Bot:NH_Schedules'))
+      .parse
+      .wikitext
+      .replace(/<\!--[\w\W]+?-->/g, '')
+      .split('\n')
+      .filter((line) => line.match(/^\s*\*.+?\-.+$/))
+      .map(line => line.replace(/^\s*\*/, '').trim())
+      .map((line) => { // parse lines
+        const event = {
+          date: '',
+          isFullDay: true,
+          startTime: '',
+          endTime: '',
+          message: ''
+        }
+
+        const [dateTimeStr, message] = line.split('-', 2).map(i => i.trim())
+        const [dateStr, timeStr] = dateTimeStr.split(' ', 2).map(i => i.trim())
+
+        event.date = dateStr
+        event.message = message
+
+        if (timeStr) {
+          const [startTime, endTime] = timeStr.split('~', 2).map(i => i.trim())
+
+          event.isFullDay = false
+          event.startTime = startTime
+          event.endTime = endTime || startTime
+        }
+
+        return event
+      })
+  }
+
+  async fetchBirthdays () {
+    return Object
+      .assign({},
+        ...((await fetchPage('Bot:NH_Birthdays'))
+          .parse
+          .wikitext
+          .replace(/<\!--[\w\W]+?-->/g, '')
+          .split('\n')
+          .filter((line) => line.match(/^\s*\*\s*.+/))
+          .map(line => line.replace(/^\s*\*/, '').trim())
+          .map(line => line.split('-', 2).map(i => i.trim()))
+          .map(([date, villagers]) => ({
+            [date]: villagers.split(',')
+          })))
+      )
+  }
+}
+
+
--- index.js
+++ index.js
@@ -1,83 +1,9 @@
 const Discord = require('discord.js')
 const dayjs = require('dayjs')
 const config = require('./config').default
-const fetchPage = require('./fetch-page').default
+const DataBag = require('./data-bag').default
 
 const client = new Discord.Client()
-
-class DataBag {
-  constructor () {
-    this.events = []
-    this.birthdays = {}
-  }
-
-  async updateAll() {
-    return Promise.all([
-      this.updateEvents(),
-      this.updateBirthdays()
-    ])
-  }
-
-  async updateEvents () {
-    this.events = await this.fetchEvents()
-  }
-
-  async updateBirthdays () {
-    this.birthdays = await this.fetchBirthdays()
-  }
-
-  async fetchEvents () {
-    return (await fetchPage('Bot:NH_Schedules'))
-      .parse
-      .wikitext
-      .replace(/<\!--[\w\W]+?-->/g, '')
-      .split('\n')
-      .filter((line) => line.match(/^\s*\*.+?\-.+$/))
-      .map(line => line.replace(/^\s*\*/, '').trim())
-      .map((line) => { // parse lines
-        const event = {
-          date: '',
-          isFullDay: true,
-          startTime: '',
-          endTime: '',
-          message: ''
-        }
-
-        const [dateTimeStr, message] = line.split('-', 2).map(i => i.trim())
-        const [dateStr, timeStr] = dateTimeStr.split(' ', 2).map(i => i.trim())
-
-        event.date = dateStr
-        event.message = message
-
-        if (timeStr) {
-          const [startTime, endTime] = timeStr.split('~', 2).map(i => i.trim())
-
-          event.isFullDay = false
-          event.startTime = startTime
-          event.endTime = endTime || startTime
-        }
-
-        return event
-      })
-  }
-
-  async fetchBirthdays () {
-    return Object
-      .assign({},
-        ...((await fetchPage('Bot:NH_Birthdays'))
-          .parse
-          .wikitext
-          .replace(/<\!--[\w\W]+?-->/g, '')
-          .split('\n')
-          .filter((line) => line.match(/^\s*\*\s*.+/))
-          .map(line => line.replace(/^\s*\*/, '').trim())
-          .map(line => line.split('-', 2).map(i => i.trim()))
-          .map(([date, villagers]) => ({
-            [date]: villagers.split(',')
-          })))
-      )
-  }
-}
 
 ;(async () => {
 
