9e667a5276dd18854a57e425dd7564aab201f05d
BinotaLIU <binota@binota.org>
2020-03-24 08:16:14 +0800

    fix days counter

--- src/scheduled-jobs/days-counter.js
+++ src/scheduled-jobs/days-counter.js
@@ -3,7 +3,7 @@ const epoch = 1584691200000
 const CH_ID = '683346217672900675'
 
 module.exports = ({ client, addJob }) => [
-  addJob('0 * * * * *', async () => {
+  addJob('0 0 0 * * *', async () => {
     const channel = await client.channels.fetch(CH_ID)
 
     const days = Math.ceil((Date.now() - epoch) / 86400000) + 1
