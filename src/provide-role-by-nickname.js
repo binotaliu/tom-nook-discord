f0606ecaf9778e3d5840280647db2d5a8ed6adb7
BinotaLIU <binota@binota.org>
2020-03-27 19:10:00 +0800

    ignore thoese who already has roles

--- src/provide-role-by-nickname.js
+++ src/provide-role-by-nickname.js
@@ -1,18 +1,28 @@
+const ROLES = {
+  pc: '575562641930452992',
+  nl: '568701794855944223',
+  nh: '546265343459590155'
+}
+
 module.exports = (member) => {
   const { nickname } = member
+
+  if (Object.values(ROLES).filter(r => member.roles.has(r))) {
+    return true
+  }
 
   switch (true) {
     case /^.+?\s*[\|｜]\s*(SW\-|)0000\-0000\-0000?$/.test(nickname): // Invalid
     default:
       return false
     case /^.+?\s*[\|｜]\s*(\d{4}\-){2}\d{3}$/.test(nickname): // Pocket
-      member.roles.add(member.guild.roles.cache.filter(i => i.name === '口袋'))
+      member.roles.add(ROLES.pc)
       return true
     case /^.+?\s*[\|｜]\s*(\d{4}\-){2}\d{4}$/.test(nickname): // 3DS NL
-      member.roles.add(member.guild.roles.cache.filter(i => i.name === '村長'))
+      member.roles.add(ROLES.nl)
       return true
     case /^.+?\s*[\|｜]\s*(SW\-|)(\d{4}\-){2}\d{4}$/.test(nickname): // Switch NH
-      member.roles.add(member.guild.roles.cache.filter(i => i.name === '島主'))
+      member.roles.add(ROLES.nh)
       return true
   }
 
