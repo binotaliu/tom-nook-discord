f730cbb8b9dfdf8708ab646a474e785d31e01bdf
BinotaLIU <binota@binota.org>
2020-03-30 00:02:50 +0800

    fix author link

--- src/listeners/ngwords.js
+++ src/listeners/ngwords.js
@@ -19,7 +19,7 @@ module.exports = ({ client, hooks, addListener }) =>
         color: 15158332,
         author: {
           name: '粗鹽偵測',
-          url: 'https://discordapp.com/channels/${message.guild.id}/${message.channel.id}/${message.id}',
+          url: `https://discordapp.com/channels/${message.guild.id}/${message.channel.id}/${message.id}`,
         },
         description: `**User:** <@${message.author.id}>\n**Channel:** <#${message.channel.id}> \`[${message.channel.name}]\`\n**Triggered Rule:** ${match}\n${message.content}`,
         timestamp: message.createdAt.toISOString(),
