5206f08697b1cfd1a4fc2d534300f60154881ade
BinotaLIU <binota@binota.org>
2020-03-29 23:56:11 +0800

    fix channel/link style

--- src/listeners/ngwords.js
+++ src/listeners/ngwords.js
@@ -2,6 +2,10 @@ const keywords = require('../../ngwords')
 
 module.exports = ({ client, hooks, addListener }) =>
   addListener('message', (message) => {
+    if (!message.guild) {
+      return
+    }
+
     const match = keywords.find(k => k.exec(message.content.trim()))
 
     if (!match) {
@@ -13,8 +17,12 @@ module.exports = ({ client, hooks, addListener }) =>
     hooks.serveillanceCentre.send({
       embeds: [{
         color: 15158332,
-        title: '粗鹽偵測',
-        description: `**User:** <@${message.author.id}>\n**Channel:** <#${message.channel.id}> `[${message.channel.name}]`\n**Triggered Rule:** ${match}\n${message.content}`,
+        author: {
+          name: '粗鹽偵測',
+          url: 'https://discordapp.com/channels/${message.guild.id}/${message.channel.id}/${message.id}',
+        },
+        description: `**User:** <@${message.author.id}>\n**Channel:** <#${message.channel.id}> \`[${message.channel.name}]\`\n**Triggered Rule:** ${match}\n${message.content}`,
+        timestamp: message.createdAt.toISOString(),
         footer: {
           text: `Message ID: ${message.id}`,
         }
