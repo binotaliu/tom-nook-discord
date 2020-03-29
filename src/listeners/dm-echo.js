c5dcf1fbe7c5e773dc1851c1f662b9b93740d83f
BinotaLIU <binota@binota.org>
2020-03-29 23:04:21 +0800

    fix default avatar

--- src/listeners/dm-echo.js
+++ src/listeners/dm-echo.js
@@ -16,7 +16,7 @@ module.exports = ({ client, hook, addListener }) =>
     const color = isSelf ? 9807270 : 15105570
     const author = {
       name: `${message.author.tag}${message.author.bot ? ' [BOT]' : ''} | ${message.author.id}`,
-      icon_url: message.author.avatar ? `https://cdn.discordapp.com/avatars/${message.author.id}/${message.author.avatar}.png` : ${message.author.defaultAvatarURL}`,
+      icon_url: message.author.avatar ? `https://cdn.discordapp.com/avatars/${message.author.id}/${message.author.avatar}.png` : message.author.defaultAvatarURL,
       url: `https://discordapp.com/users/${message.author.id}`
     }
     const footer = { text: `CH ID: ${message.channel.id} | MSG ID: ${message.id}` }
