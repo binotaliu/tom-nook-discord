07e12992e93c8e4057edeae0aff68afdc91551b0
BinotaLIU <binota@binota.org>
2020-03-29 17:55:28 +0800

    add inrole command

--- src/commands/index.js
+++ src/commands/index.js
@@ -1,10 +1,12 @@
 const autokick = require('./autokick')
 const say = require('./say')
 const del = require('./del')
+const inrole = require('./inrole')
 
 module.exports = [
   autokick,
   say,
-  del
+  del,
+  inrole
 ]
 
---
+++ src/commands/inrole.js
@@ -0,0 +1,19 @@
+const { splitMessage } = require('discord.js')
+
+const chunk = (arr, size) => Array.from({ length: Math.ceil(arr.length / size) }, (v, i) => arr.slice(i * size, i * size + size))
+
+module.exports = ({ config, client, addCommand }) =>
+  addCommand('inrole', async (triggerMsg, role, inRawText) => {
+    const roleId = role.match(/<@\&?(\d+)>/i).slice(1).find(() => true)
+
+    console.log(roleId)
+
+    const list = (await triggerMsg.guild.members.fetch()).array().filter(i => i.roles.cache.has(roleId)).map(i => i.id)
+
+    const listMessages = chunk(list, 8).map(l => l.map(i => `<@${i}>`).join(','))
+
+    for (const msg of listMessages) {
+      triggerMsg.channel.send(inRawText === 'yes' ? `\`${msg}\`` : msg)
+    }
+  })
+
