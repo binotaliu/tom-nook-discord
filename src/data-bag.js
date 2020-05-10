0828768a6a4a6e76899d79de72052ef33526c556
BinotaLIU <me@binota.org>
2020-05-10 14:38:04 +0800

    fix axios import

--- src/data-bag.js
+++ src/data-bag.js
@@ -1,3 +1,5 @@
+const axios = require('axios')
+
 const fetchPage = async (pageTitle) =>
   (await axios.get(`https://acnh.tw/api.php?action=parse&formatversion=2&page=${pageTitle}&format=json&prop=wikitext`))
     .data
