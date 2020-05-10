07ad160b077491ff3dbed8f3341662009b7e5367
BinotaLIU <me@binota.org>
2020-05-10 18:39:05 +0800

    fix missing app in roles-confirmation

--- src/modules/roles-confirmation/index.js
+++ src/modules/roles-confirmation/index.js
@@ -4,7 +4,7 @@ const ROLE_ID_NL = '568701794855944223'
 const ROLE_ID_NS = '546265343459590155'
 const ROLE_ID_PC = '575562641930452992'
 
-module.exports = ({ addJob }) => [
+module.exports = ({ app, addJob }) => {
   addJob(
     '0,30 * * * * *',
     () =>
@@ -30,4 +30,4 @@ module.exports = ({ addJob }) => [
             })
         })
   )
-]
+}
