751c274aee39f182e0640b8ac720c8e632480791
BinotaLIU <me@binota.org>
2020-05-12 17:59:16 +0800

    fix del got wrong channel

--- src/modules/del/index.js
+++ src/modules/del/index.js
@@ -1,8 +1,6 @@
 module.exports = ({ app, addCommand }) =>
   addCommand('del', async (triggerMsg, channelId, id) => {
     const channel = await app.client.channels.fetch(channelId)
-    console.log(channel)
-    const message = await app.channel.messages.fetch(id)
-    console.log(message)
+    const message = await channel.messages.fetch(id)
     message.delete()
   })
