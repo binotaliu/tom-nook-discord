cb612783351b404c29c80dcf537ee61a02b84386
BinotaLIU <me@binota.org>
2020-05-12 17:57:37 +0800

    fix wrong way to create dmch

--- src/modules/nickname-changes-watcher/index.js
+++ src/modules/nickname-changes-watcher/index.js
@@ -7,7 +7,7 @@ module.exports = ({ addListener }) =>
     }
 
     const result = provideRoleByNickname(updated)
-    const dmch = await updated.createDM
+    const dmch = await updated.createDM()
 
     if (result === true) {
       dmch.send(`${updated.user.username} 你好！已經幫你新增了權限，請記得按照指南中的說明領取水果哦！（若領到可忽略）`)
@@ -15,7 +15,6 @@ module.exports = ({ addListener }) =>
     }
 
     if (result === false) {
-      const dmch = await updated.createDM()
       dmch.send(`${updated.user.username} 你好！你剛才在 ${old.guild.name} 上修改了暱稱，但似乎未在暱稱上找到你的 FC，請參考 <#546258423398793217> 設定暱稱。`)
     }
   })
